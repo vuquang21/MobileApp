a9175a62aee62c2b4a5b167ece987f2ccfef13da
Quang <qvu2k1@gmail.com>
2021-11-07 17:49:20 +0700

    Main_ui_template

--- Podcast/app/src/main/java/com/example/podcast/Main.java
+++ Podcast/app/src/main/java/com/example/podcast/Main.java
@@ -4,22 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-
-public class Login extends AppCompatActivity {
-
-
-
+public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
-
-
-
+        setContentView(R.layout.main);
     }
-
-
-
 }
